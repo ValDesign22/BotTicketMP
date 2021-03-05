575be0698609182cc32d3fc0d547493f13e59c82
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-03-05 12:40:49 +0100

    Update message.js
    Send images is now possible

--- events/message.js
+++ events/message.js
@@ -6,20 +6,13 @@ module.exports = async(client, message) => {
 
     //Ticket mp system
     if(message.channel.type === "dm") {
-        //Get image from message
-        let Attachment = (message.attachments).array();
-        
-        //Message to send to support
         const msg = message.content;
-        
-        //Guild support
+
         const guild = client.guilds.cache.find(g => g.id === client.config.guild);
-        
-        //Category of all tickets
+
         let categorie = guild.channels.cache.find(c => c.name == "Tickets" && c.type == "category");
         if (!categorie) categorie = await guild.channels.create("Tickets", { type: "category", position: 1 }).catch(e => { return console.error(e) });
-        
-        //If the ticket channel not exist
+
         if (!guild.channels.cache.find(c => c.name === `${message.author.id}-mp`)) {
             guild.channels.create(`${message.author.id}-mp`, {
                 permissionOverwrites: [
@@ -42,10 +35,12 @@ module.exports = async(client, message) => {
                 .setDescription(`Utilisateur: ${message.author.tag}\nID: ${message.author.id}`)
                 .setFooter("Merci de cliquer sur 🔒 pour fermer le ticket.")
                 .addField("Sa question:", msg)
-                
-                //If image then setImage the embed
-                if (Attachment[0].url.toUpperCase().endsWith('.PNG') || Attachment[0].url.toUpperCase().endsWith('.JPG') || Attachment[0].url.toUpperCase().endsWith('.JPEG') || Attachment[0].url.toUpperCase().endsWith('.WEBP')) {
-                    e.setImage(Attachment[0].url)
+
+                if (message.attachments.size > 0) {
+                    e.setImage(message.attachments.first().attachment)
+                }
+                else {
+                    e.setImage(null)
                 }
 
                 ch.send(e)
@@ -54,7 +49,6 @@ module.exports = async(client, message) => {
                 })
             })
         }
-        //If the ticket channel exist
         else {
             const channelTicket = guild.channels.cache.find(c => c.name === `${message.author.id}-mp`)
 
@@ -63,42 +57,50 @@ module.exports = async(client, message) => {
             .setColor("#2F3136")
             .addField("Sa question:", msg)
 
+            if (message.attachments.size > 0) {
+                e.setImage(message.attachments.first().attachment)
+            }
+            else {
+                e.setImage(null)
+            }
+
             channelTicket.send(e)
         }
     }
-    //In the support guild
     else {
-        //If the channel name ends with "-mp"
         if (message.channel.name.endsWith("-mp")) {
-            //The staff message
             const msg = message.content
-            
-            //Delete the staff message
-            message.delete()
-            
-            //Staff message in an embed
-            const e2 = new Discord.MessageEmbed()
-            .setTitle(message.author.tag)
-            .setColor("#2F3136")
-            .setDescription(msg)
 
-            message.channel.send(e2)
-            
-            //Define the user
             const user = await client.users.fetch(`${message.channel.topic}`)
-            
-            //Embed to send to user
+
             const e = new Discord.MessageEmbed()
             .setTitle("Réponse du staff")
             .setColor("#2F3136")
             .addField(`${message.author.tag}`, msg)
 
+            const e2 = new Discord.MessageEmbed()
+            .setTitle(message.author.tag)
+            .setColor("#2F3136")
+            .setDescription(msg)
+
+            if (message.attachments.size > 0) {
+                e.setImage(message.attachments.first().attachment)
+                e2.setImage(message.attachments.first().attachment)
+            }
+            else {
+                e.setImage(null)
+                e2.setImage(null)
+            }
+            
+            message.channel.send(e2)
+
             await user.send(e)
             .then(msg => {
                 msg.react("📥")
             })
+
+            message.delete()
         }
-        //If not mp channel or ticket channel
         else {
             //Ignore message starting with not prefix
             if (message.content.indexOf(client.config.prefix) !== 0) return;
