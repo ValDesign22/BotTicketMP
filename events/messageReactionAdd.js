427289fa309a885e118eb034be807431691eef95
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-03-05 12:41:41 +0100

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -12,7 +12,19 @@ module.exports = async (client, reaction, user) => {
         //Close a ticket if the channel name ends with -mp
         if (reaction.emoji.name === "🔒") {
             if (message.channel.name.endsWith("-mp")) {
-                message.channel.delete()
+                const user = await client.users.fetch(`${message.channel.topic}`);
+
+                const guildSupport = client.guilds.cache.find(g => g.id === `${client.config.guild}`);
+                const channelTicketReaction = guildSupport.channels.cache.find(c => c.name === "🎟︙tickets");
+
+                const e = new Discord.MessageEmbed()
+                .setTitle("Ticket fermé")
+                .setColor("#2F3136")
+                .setDescription(`Ton ticket à été fermé par le support.\nSi tu as d'autres questions merci d'ouvrir un ticket içi: <#${channelTicketReaction.id}> ou en m'envoyant un message privé.`)
+
+                await user.send(e);
+
+                message.channel.delete();
             }
             else {
                 return;
