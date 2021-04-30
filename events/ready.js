d33c024357794f40c9aeb63c4d8a41024c583fc2
ValRedstone <73596429+ValRedstone@users.noreply.github.com>
2021-04-30 18:30:02 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,6 @@
 module.exports = (client) => {
     //Log in the console when the bot is online
     console.log(`${client.user.tag} is now ready !`);
+    
+    client.user.setActivity("MP moi!")
 }
